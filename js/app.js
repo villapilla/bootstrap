2a9a5288ccb3833aad711ee2625b944a1a12d240
dav <dvillaluenga@gmail.com>
2016-02-07 13:58:02 +0100

    cabecera ok

--- js/app.js
+++ js/app.js
@@ -2,23 +2,31 @@ $(window).ready(function() {
     var sideslider = $('[data-toggle=collapse-side]'),
         sel = sideslider.attr('data-target'),
         sel2 = sideslider.attr('data-target-2'),
+        bodyWidth,
         contentPosition;
-
     sideslider.click(function(event) {
         $(sel).toggleClass('in');
         $(sel2).toggleClass('out');
     });
+    //Resize dropdownMenu
+    /*$(window).on('resize', function () {
+        bodyWidth = document.body.clientWidth;
+        if(bodyWidth > 768 && bodyWidth < 992) {
+            $("#login ul").addClass("dropdown-menu");
+        } else {
+            $("#login ul").removeClass("dropdown-menu");
+        }
+    })*/
     //Navigation menu follow
     $(document).on('scroll', function() {
         contentPosition = $('header').height() + 25;
         var stop = Math.round();
-
         if (window.scrollY > contentPosition) {
-            $('main nav').addClass('navbar navbar-fixed-top navbar-inverse');
+            $('#nav_main').addClass('navbar navbar-fixed-top navbar-inverse');
         } else {
-            $('main nav').removeClass("navbar navbar-fixed-top");
+            $('#nav_main').removeClass("navbar navbar-fixed-top");
+            $("main nav .side-collapse").css({top : (183 - window.scrollY) + "px"});
         }
-
     });
 
 });
