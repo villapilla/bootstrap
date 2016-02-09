52df6bb6110a8279236c78f4472d5213db864dae
dav <dvillaluenga@gmail.com>
2016-02-09 12:55:07 +0100

    carrusel hecho, menu hecho, solo queda hover productos

--- js/app.js
+++ js/app.js
@@ -3,7 +3,72 @@ $(window).ready(function() {
         sel = sideslider.attr('data-target'),
         sel2 = sideslider.attr('data-target-2'),
         bodyWidth,
-        contentPosition;
+        contentPosition,
+        timeoutCarrusel = createSetInterval();
+
+        function createSetInterval() {
+            return window.setInterval(function () {
+            if($("#carrusel1").position().left < 600) {
+                $("#carrusel1").animate({
+                    left : "-101%"
+                }, 2000 , function () {
+                    $("#carrusel1").css("left", "95%"); 
+                    $("#boton_carrusel2").css("opacity", 1);
+                    $("#boton_carrusel1").css("opacity", 0.5);   
+                });
+                $("#carrusel2").animate({
+                    left : "3%"
+                }, 2000);
+            } else {
+                $("#carrusel1").animate({
+                    left : "0"
+                }, 2000);
+                $("#carrusel2").animate({
+                    left : "-100%"
+                }, 2000, function () {
+                    $("#carrusel2").css("left", "101%"); 
+                    $("#boton_carrusel1").css("opacity", 1);
+                    $("#boton_carrusel2").css("opacity", 0.5);   
+                });
+            }
+            }, 30000);
+        }   
+    $("#boton_carrusel1").click(function (e) {
+        $("#boton_carrusel1").css("opacity", 1);
+        $("#boton_carrusel2").css("opacity", 0.5);
+        if($("#carrusel1").position().left > 600) {
+            $("#carrusel1").animate({
+                left : "0"
+            }, 2000);
+            $("#carrusel2").animate({
+                left : "-101%"
+            }, 2000, function () {
+                $("#carrusel2").css("left", "101%");     
+            });
+        }
+        clearInterval(timeoutCarrusel);
+        timeoutCarrusel = createSetInterval();
+    });
+    $("#boton_carrusel2").click(function (e) {
+        $("#boton_carrusel2").css("opacity", 1);
+        $("#boton_carrusel1").css("opacity", 0.5);
+        if($("#carrusel1").position().left < 600) {
+            $("#carrusel1").animate({
+                left : "-101%"
+            }, 2000 , function () {
+                $("#carrusel1").css("left", "101%");     
+            });
+            $("#carrusel2").animate({
+                left : "3%"
+            }, 2000);
+        }
+        clearInterval(timeoutCarrusel);
+        timeoutCarrusel = createSetInterval();
+    });
+    if (window.screen.availWidth < 768) {
+        $("main nav .side-collapse").css({top : $("#descripcion").offset().top + "px"});
+    }
+    
     sideslider.click(function(event) {
         $(sel).toggleClass('in');
         $(sel2).toggleClass('out');
@@ -20,13 +85,15 @@ $(window).ready(function() {
     //Navigation menu follow
     $(document).on('scroll', function() {
         contentPosition = $('header').height() + 25;
-        var stop = Math.round();
         if (window.scrollY > contentPosition) {
-            $('#nav_main').addClass('navbar navbar-fixed-top navbar-inverse');
+            $('#nav_main').addClass('navbar navbar-fixed-top navbar-inverse linear_gradient');
+            $("#nav_main .container").addClass("linear_gradient");
+            $("main nav .side-collapse").css({top : $(".navbar-header")[0].clientHeight + "px"});
         } else {
-            $('#nav_main').removeClass("navbar navbar-fixed-top");
-            $("main nav .side-collapse").css({top : (183 - window.scrollY) + "px"});
+            $('#nav_main').removeClass("navbar navbar-fixed-top linear_gradient");
+            $("#nav_main .container").removeClass("linear_gradient");
+           
+            $("main nav .side-collapse").css({top : $("#descripcion").offset().top + "px"});
         }
     });
-
 });
